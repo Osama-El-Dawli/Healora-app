d982734d3338907992d522a3bbcb4e02fc1064e6
Osama Said <osamasaeed283@gmail.com>
2025-10-13 02:47:28 +0300

    fix: resolve merge conflicts in CustomProfileAvatar widget

--- lib/core/widgets/custom_profile_avatar.dart
+++ lib/core/widgets/custom_profile_avatar.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-=======
->>>>>>> d169923088cd0a4f18e34295af333293f35c0660
 
 class CustomProfileAvatar extends StatelessWidget {
   const CustomProfileAvatar({super.key, required this.imageUrl});
@@ -12,15 +9,9 @@ class CustomProfileAvatar extends StatelessWidget {
   Widget build(BuildContext context) {
     return CircleAvatar(
       backgroundColor: Color(0xffEAF2F7),
-<<<<<<< HEAD
       radius: 23.r,
       child: CircleAvatar(
         radius: 15.r,
-=======
-      radius: 23,
-      child: CircleAvatar(
-        radius: 15,
->>>>>>> d169923088cd0a4f18e34295af333293f35c0660
         backgroundImage: AssetImage(imageUrl),
       ),
     );
