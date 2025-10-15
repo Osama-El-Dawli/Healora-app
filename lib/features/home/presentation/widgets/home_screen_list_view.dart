c15d72b2e3169a8f58eb236ab678bee8823b8b82
mohamed <mohamedashrf016@gmail.com>
2025-10-15 19:51:24 +0300

    feat: implement home screen and onboarding page layout with localization support

--- lib/features/home/presentation/widgets/home_screen_list_view.dart
+++ lib/features/home/presentation/widgets/home_screen_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:healora/features/home/data/models/home_screen_model.dart';
@@ -6,22 +7,22 @@ import 'package:healora/features/home/presentation/widgets/home_screen_cards.dar
 class HomeScreenListView extends StatelessWidget {
   const HomeScreenListView({super.key});
 
-  static const List<HomeScreenModel> items = [
+  static List<HomeScreenModel> items = [
     HomeScreenModel(
       image: 'assets/images/home_page1.png',
-      title: 'Medical History',
+      title: 'Medical History'.tr(),
     ),
     HomeScreenModel(
       image: 'assets/images/home_page2.png',
-      title: 'Lab Results',
+      title: 'Lab Results'.tr(),
     ),
     HomeScreenModel(
       image: 'assets/images/home_page3.png',
-      title: 'Choose a Doctor',
+      title: 'Choose a Doctor'.tr(),
     ),
     HomeScreenModel(
       image: 'assets/images/home_page4.png',
-      title: 'Your Diet Chart',
+      title: 'Your Diet Chart'.tr(),
     ),
     HomeScreenModel(image: 'assets/images/home_page5.png', title: 'ChatBot'),
   ];
