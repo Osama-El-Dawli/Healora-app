7d6e7d32a1df3abee90c23b5bd7cb30cc74171c3
Osama Saeid <osamasaeed283@gmail.com>
2025-10-24 22:23:58 +0300

    feat: Refactor select doctor feature with new model and UI components

--- lib/features/select_doctor/presentation/screens/select_doctor_screen.dart
+++ lib/features/select_doctor/presentation/screens/select_doctor_screen.dart
@@ -1,47 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:healora/core/routes/routes.dart';
-import 'package:healora/features/select_doctor/presentation/widgets/select_doctor_item.dart';
-
-import '../../../../core/theme/app_colors.dart';
+import 'package:healora/features/select_doctor/presentation/widgets/select_doctor_screen_body.dart';
 
 class SelectDoctorScreen extends StatelessWidget {
-  static const String routeName = AppRoutes.selectDoctorScreen;
-List<DoctorItem> doctors=[
-DoctorItem(name: "mariam", specialty: "teeth", icon: Icons.message, image: "profile"),
-  DoctorItem(name: "mariam", specialty: "teeth", icon: Icons.message, image: "profile"),
-  DoctorItem(name: "mariam", specialty: "teeth", icon: Icons.message, image: "profile"),
-  DoctorItem(name: "mariam", specialty: "teeth", icon: Icons.message, image: "profile"),
-  DoctorItem(name: "mariam", specialty: "teeth", icon: Icons.message, image: "profile"),
-
-];
+  const SelectDoctorScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(color: AppColors.lightGray,
-                  borderRadius: BorderRadius.circular(15)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                Text("Choose a doctor",
-                  style: Theme.of(context).textTheme.titleLarge,),
-                Image.asset("assets/images/home_page3.png",
-                  width: MediaQuery.sizeOf(context).width*.3,)
-              ],),
-            ),
-            Expanded(
-              child: ListView.separated(separatorBuilder: (_,index)=>SizedBox(height: 10,),
-                itemBuilder: (_,index)=>doctors[index]
-                ,itemCount: doctors.length,),
-            ),
-          ],
-        ),
-      ),
+      body: SelectDoctorScreenBody(),
     );
   }
 }
+
--- lib/features/select_doctor/presentation/widgets/select_doctor_item.dart
+++ lib/features/select_doctor/presentation/widgets/select_doctor_item.dart
@@ -1,36 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:healora/core/theme/app_colors.dart';
+import 'package:healora/features/select_doctor/data/models/select_doctor_model.dart';
 
-class DoctorItem extends StatelessWidget {
-  String name;
-  String specialty;
-  IconData icon;
-  String image;
-  DoctorItem({required this.name,
-    required this.specialty,
-    required this.icon,
-    required this.image});
+class SelectDoctorItem extends StatelessWidget {
+  final SelectDoctorModel model;
+  const SelectDoctorItem({super.key, required this.model});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10),
-      decoration: BoxDecoration(color: AppColors.lightGray,
-      borderRadius: BorderRadius.circular(15)),
+      decoration: BoxDecoration(
+        color: AppColors.lightGray,
+        borderRadius: BorderRadius.circular(12.r),
+      ),
       child: ListTile(
-        leading: CircleAvatar(
-          radius: 30,
-          backgroundImage: AssetImage("assets/images/${image}.png"),
+        leading: CircleAvatar(radius: 28.r, backgroundImage: AssetImage(model.image)),
+        title: Text(
+          model.name,
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+            fontSize: 18.sp,
+            fontWeight: FontWeight.w500,
+          ),
         ),
-        title: Text(name,
-          style: Theme.of(context).textTheme.titleMedium,),
-        subtitle: Text(specialty,
-          style: Theme.of(context).textTheme.labelLarge!.copyWith(color: AppColors.primary),
-         ),
-        trailing: Icon(icon,color: AppColors.secondary,size: 30,),
-        onTap: () {},
+        subtitle: Text(
+          model.specialty,
+          style: Theme.of(context).textTheme.labelLarge!.copyWith(
+            color: AppColors.hintColor,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.chat, color: AppColors.secondary, size: 24.sp),
+        ),
       ),
     );
   }
 }
-
